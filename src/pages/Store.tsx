d058ea7735350da2d86898079ebabaac36c69371
Abishake-5 <ghanabiswa5@gmail.com>
2022-12-14 05:44:30 -0500

    First Commit

---
+++ src/pages/Store.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import storeItems from "../data/items.json"
+import {Row, Col} from "react-bootstrap"
+export  function Store() {
+  return (
+  <>
+    <h1>Store</h1>
+    <Row>
+      <Col></Col>
+    </Row>
+  </>
+  )
+}
